5126f9b87c841abb842c713e49c241f1ef19f44e
jeon <jeon0428k@gmail.com>
2025-11-14 00:01:53 +0900

    Init Commit

---
+++ src/main/java/com/springapp/mvc/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.springapp.mvc.controller;
+
+import com.springapp.mvc.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TestController {
+
+    @RequestMapping(value = "/hello",
+            method = RequestMethod.GET,
+            produces="application/json;charset=UTF-8")
+    public void hello(ModelMap model) {
+        model.addAttribute("user", new User());
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/pages/hello.jsp
@@ -0,0 +1,12 @@
+<html>
+<body>
+	<h1>${message}</h1>
+    test 11
+    <script>
+
+        const val = "aaa";
+        console.log('val', val);
+
+    </script>
+</body>
+</html>
